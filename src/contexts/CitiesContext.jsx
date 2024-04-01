6dd97cffdcc1f5d5ccd0c7288bbf6daacb1162d9
Abdulrhman El-Daly <abd911daly@gmail.com>
2024-04-02 00:27:44 +0200

    fixed json server bug

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -1,8 +1,84 @@
 import { createContext, useContext, useEffect, useState } from "react";
-
+import uuid from "react-uuid";
 const CitiesContext = createContext();
-const BASE_URL = "http://localhost:9000";
+
 // eslint-disable-next-line react/prop-types
+
+const data = [
+  {
+    cityName: "Cairo",
+    country: "Egypt",
+    emoji: "🇪🇬",
+    date: "2024-10-31T15:59:59.138Z",
+    notes: "This is where I live 😊🙋‍♂️",
+    position: {
+      lat: 29.98077,
+      lng: 30.913933,
+    },
+    id: "13930385",
+  },
+  {
+    cityName: "Lisbon",
+    country: "Portugal",
+    emoji: "🇵🇹",
+    date: "2027-10-31T15:59:59.138Z",
+    notes: "My favorite city so far!",
+    position: {
+      lat: 38.727881642324164,
+      lng: -9.140900099907554,
+    },
+    id: "73930385",
+  },
+  {
+    cityName: "Madrid",
+    country: "Spain",
+    emoji: "🇪🇸",
+    date: "2027-07-15T08:22:53.976Z",
+    notes: "",
+    position: {
+      lat: 40.46635901755316,
+      lng: -3.7133789062500004,
+    },
+    id: "17806751",
+  },
+  {
+    cityName: "Berlin",
+    country: "Germany",
+    emoji: "🇩🇪",
+    date: "2027-02-12T09:24:11.863Z",
+    notes: "Amazing 😃",
+    position: {
+      lat: 52.53586782505711,
+      lng: 13.376933665713324,
+    },
+    id: "98443197",
+  },
+  {
+    id: "cb41",
+    cityName: "Antalya",
+    country: "Turkiye",
+    emoji: "🇹🇷",
+    date: "2023-11-09T18:06:05.000Z",
+    notes: "Best Trip EVER !! :)",
+    position: {
+      lat: "36.88511287236025",
+      lng: "30.751419067382812",
+    },
+  },
+  {
+    id: "3c06",
+    cityName: "Marsa Maţruḩ",
+    country: "Egypt",
+    emoji: "🇪🇬",
+    date: "2024-04-01T22:00:38.094Z",
+    notes: "dada",
+    position: {
+      lat: "30.14512718337613",
+      lng: "26.828613281250004",
+    },
+  },
+];
+
 function CitiesProvider({ children }) {
   const [cities, setCities] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
@@ -12,8 +88,6 @@ function CitiesProvider({ children }) {
     async function fetchCities() {
       try {
         setIsLoading(true);
-        const res = await fetch(`${BASE_URL}/cities`);
-        const data = await res.json();
 
         setCities(data);
       } catch {
@@ -28,10 +102,9 @@ function CitiesProvider({ children }) {
   async function getCity(id) {
     try {
       setIsLoading(true);
-      const res = await fetch(`${BASE_URL}/cities/${id}`);
-      const data = await res.json();
-
-      setCurrentCity(data);
+      const selectedCity = data.find((city) => city.id === id);
+      console.log(selectedCity);
+      setCurrentCity(selectedCity);
     } catch {
       alert("Error fetching data");
     } finally {
@@ -42,15 +115,11 @@ function CitiesProvider({ children }) {
   async function createCity(newCity) {
     try {
       setIsLoading(true);
-      const res = await fetch(`${BASE_URL}/cities`, {
-        method: "POST",
-        body: JSON.stringify(newCity),
-        headers: { "Content-Type": "application/json" },
-      });
-      const data = await res.json();
 
-      setCities((cities) => [...cities, data]);
-      setCurrentCity(data);
+      const id = uuid();
+      const cityWithId = { ...newCity, id };
+      setCities((cities) => [...cities, cityWithId]);
+      setCurrentCity(cityWithId);
     } catch {
       alert("Error adding a city");
     } finally {
@@ -61,9 +130,6 @@ function CitiesProvider({ children }) {
   async function deleteCity(id) {
     try {
       setIsLoading(true);
-      await fetch(`${BASE_URL}/cities/${id}`, {
-        method: "DELETE",
-      });
 
       setCities((cities) => cities.filter((city) => city.id !== id));
     } catch {
